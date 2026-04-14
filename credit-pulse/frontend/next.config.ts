6c4b85714e016f519b37885c9c9c821f71b86f73
Victor <victor.id1993@gmail.com>
2026-04-14 10:55:45 -0400

    fix images issues on production

--- credit-pulse/frontend/next.config.ts
+++ credit-pulse/frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
